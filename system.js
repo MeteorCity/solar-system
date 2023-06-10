6555aff0d822bef54bfbd949fc56ba1e3ff66cb0
John Talghader <john.talghader@gmail.com>
2023-06-09 22:23:37 -0500

    Third Commit

--- system.js
+++ system.js
@@ -49,22 +49,46 @@ function displayText(planetElement) {
     }
 }
 
-/*
-planetElement.addEventListener("mousemove", onMouseMove);
+const sun = document.querySelector(".sun");
+const containers = document.querySelectorAll(".container");
+const ballPlanets = document.querySelectorAll(".container .balls");
+let in2D = true;
 
-function onMouseMove() {
-    let isMouseOver = false;
+function changeTo2D() {
+    if (in2D) {
+        return;
+    }
 
-    planetElement.addEventListener("mouseover", function flag() {
-        isMouseOver = true;
-
-        if (!isMouseOver) {
-            planetDescription.style.opacity = 0;
-            planetElement.removeEventListener("mousemove", onMouseMove);
-        }
-
-        planetElement.removeEventListener("mouseover", flag);
-        planetElement.removeEventListener("mousemove", onMouseMove);
+    sun.style.zIndex = 9999;
+    containers.forEach((container) => {
+        container.style.transform = "translate(-50%, -50%)";
+        container.style.padding = "12px";
     });
+    ballPlanets.forEach((ballPlanet) => {
+        ballPlanet.style.transform = "";
+    });
+
+    changeSpeed(1);
+
+    in2D = true;
 }
-*/
+
+function changeTo3D() {
+    if (!in2D) {
+        return;
+    }
+
+    sun.style.zIndex = -1;
+    containers.forEach((container) => {
+        container.style.transform = "translate(-50%, -50%) rotateX(77deg)";
+        container.style.padding = "50px";
+    });
+    ballPlanets.forEach((ballPlanet) => {
+        ballPlanet.style.transform = "rotateY(-77deg) rotateZ(30deg) scale(4)";
+    });
+
+    changeSpeed(0);
+
+    in2D = false;
+}
+
